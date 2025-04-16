885bb924e171d82339a54acdbdb3548aeb1f2a86
Keili Brahms <iamkeili@yahoo.com>
2025-04-15 18:26:20 -0700

    Added move speed

--- Assets/Scripts/Player_Controller.cs
+++ Assets/Scripts/Player_Controller.cs
@@ -3,11 +3,13 @@ using UnityEngine;
 public class Player_Controller : MonoBehaviour
 {
     public float rotSpeed;
+    public float moveSpeed;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rotSpeed = 10f;
+        moveSpeed = 10f;
     }
 
     // Update is called once per frame
@@ -21,5 +23,26 @@ public class Player_Controller : MonoBehaviour
         {
             transform.Rotate(Vector3.up, rotSpeed * Time.deltaTime);
         }
+
+        //MOVEMENT
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(-Vector3.forward * moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(-Vector3.left * moveSpeed * Time.deltaTime);
+        }
+
     }
+
+
 }
